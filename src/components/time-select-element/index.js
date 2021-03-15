a7bcc86dde688bd9bbb4a563433cf5c9cbc2b706
AndreyPanovA <a.andrey.panov@gmail.com>
2021-03-16 00:17:10 +0300

    styling finished

--- src/components/time-select-element/index.js
+++ src/components/time-select-element/index.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React,{ useState } from "react"
 import TimePicker from 'react-time-picker'
+// import {Timepicker} from 'react-timepicker';
+// import 'react-timepicker/timepicker.css'
 import Btn from "../btn";
 import cls from "./style.module.scss"
 const TimeSelectElement=({title})=> {
+    const [value, onChange] = useState('10:00');
     return (
         <div className={cls.container}>
             <Btn title={title} />
+            {/* <TimePicker
+        onChange={onChange}
+        value={value}
+      /> */}
+    
         </div>
       
     )
